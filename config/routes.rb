279e03cfca4017377006a75da3a7d96a74568eb9
felipesantos94 <felipe.fars94@gmail.com>
2023-08-09 17:40:17 +0200

    Create root index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  # GET /about
-  get "about", to:"about#index"
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root, to: "main#index"
+
+  get "about", to:"about#index"
 end
