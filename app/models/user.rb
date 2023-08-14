005040d4aa5c704a4766c7436cddd49d6348eab3
felipesantos94 <felipe.fars94@gmail.com>
2023-08-14 14:28:28 +0200

    User schema and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: { with: ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g, message: "Must have a valid e-mail format" }
 end
