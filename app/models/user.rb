df15f0f276463b3cb0a9b9b902dfc8d510381a8e
felipesantos94 <felipe.fars94@gmail.com>
2023-08-17 19:46:39 +0200

    Fixing regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g, message: "Must have a valid e-mail format" }
+  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/, message: "Must have a valid e-mail format" }
 end
